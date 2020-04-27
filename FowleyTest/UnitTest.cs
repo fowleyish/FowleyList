e4ddc923f28cd36c3c01a329bbd923003c8d26ed
Fowley <alexanderfowle@gmail.com>
2020-04-27 13:59:16 -0500

    Add project files.

---
+++ FowleyTest/UnitTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FowleyList;
+
+namespace FowleyTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
